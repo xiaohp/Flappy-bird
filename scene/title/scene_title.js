799660b1f0e668eb9ba0ff63117e38e5ddf18a40
Xiao Haiping <xiaohp1993@gmail.com>
2017-08-18 15:24:06 +0800

    add config control

--- scene/title/scene_title.js
+++ scene/title/scene_title.js
@@ -4,7 +4,7 @@ class Pipes {
         this.pipes = []
         this.pipeSpace = 150
         this.管子横向间距 = 200
-        this.columsOfPipe = 3
+        this.columnsOfPipe = 3
         for (var i = 0; i < 3; i++) {
             var p1 = GuaImage.new(game, 'pipe')
             p1.x = 500 + i * this.管子横向间距
@@ -12,7 +12,7 @@ class Pipes {
             var p2 = GuaImage.new(game, 'pipe')
             p2.flipY = true
             p2.x = p1.x
-            
+
             this.resetPipesPositoin(p1, p2)
 
             this.pipes.push(p1)
@@ -26,11 +26,22 @@ class Pipes {
         p1.y = randomBetween(-200, 0)
         p2.y = p1.y + p1.h + this.pipeSpace
     }
+    debug() {
+        this.管子横向间距 = config.管子横向间距.value
+        this.pipeSpace = config.pipe_space.value
+    }
     update() {
-        for (var p of this.pipes) {
-            p.x -= 5
-            if (p.x < -100) {
-                p.x += this.管子横向间距 * this.columsOfPipe
+        for (var i = 0; i < this.pipes.length; i += 2) {
+            var p1 = this.pipes[i]
+            var p2 = this.pipes[i + 1]
+            p1.x -= 5
+            p2.x -= 5
+            if (p1.x < -100) {
+                p1.x += this.管子横向间距 * this.columnsOfPipe
+            }
+            if (p2.x < -100) {
+                p2.x += this.管子横向间距 * this.columnsOfPipe
+                this.resetPipesPositoin(p1, p2)
             }
         }
     }
@@ -45,13 +56,11 @@ class Pipes {
 
             var scaleX = p.flipX ? -1 : 1
             var scaleY = p.flipY ? -1 : 1
-            log('debug flip', p.flipX, p.flipY, scaleX, scaleY)
             context.scale(scaleX, scaleY)
             context.rotate(p.rotation * Math.PI / 180)
             context.translate(-w2, -h2)
 
             context.drawImage(p.texture, 0, 0)
-
             context.restore()
         }
     }
@@ -81,6 +90,7 @@ class SceneTitle extends GuaScene {
         }
         this.skipCount = 4
         // bird
+        this.birdSpeed = 2
         var b = GuaAnimation.new(game)
         b.x = 100
         b.y = 200
@@ -88,6 +98,9 @@ class SceneTitle extends GuaScene {
         this.addElement(b)
 
         this.setupInputs()
+    }
+    debug() {
+        this.birdSpeed = config.bird_speed.value
     }
 
     update() {
@@ -108,10 +121,10 @@ class SceneTitle extends GuaScene {
         var self = this
         var b = this.bird
         self.game.registerAction('a', function(keyStatus) {
-            b.move(-2, keyStatus)
+            b.move(-self.birdSpeed, keyStatus)
         })
         self.game.registerAction('d', function(keyStatus) {
-            b.move(2, keyStatus)
+            b.move(self.birdSpeed, keyStatus)
         })
         self.game.registerAction('j', function(keyStatus) {
             b.jump()
